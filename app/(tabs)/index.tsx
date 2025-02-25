9896e59e65d47e5ef14bce648ff921c236cb9cf3
neeraj-badam <neeraj.badam@gmail.com>
2025-02-25 14:12:10 -0500

    ActivityIndicator

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,13 @@
-import { View, StatusBar } from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
 
 export default function App(){
   
   return (
     <View style={{flex: 1, backgroundColor:'plum', padding: 60}}>
-      {/* The StatusBar is specific to Android devices, backgroundColor works for Android and remaining props work for both iOS and Android */}
-      <StatusBar backgroundColor='green' barStyle='light-content' hidden={false}/>
-      
+      <ActivityIndicator/>
+      <ActivityIndicator size={'large'}/>
+      <ActivityIndicator size={'large'} color={'red'}/>
+      <ActivityIndicator size={'large'} color={'red'} animating={false}/>
     </View>
   );
 }
