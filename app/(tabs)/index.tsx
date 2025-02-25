efd7d4d40f4104f1034f37f6df0861bfee463457
neeraj-badam <neeraj.badam@gmail.com>
2025-02-25 14:22:52 -0500

    Alerts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,32 @@
-import { View, ActivityIndicator } from 'react-native';
+import { View, Alert, Button } from 'react-native';
 
 export default function App(){
   
   return (
     <View style={{flex: 1, backgroundColor:'plum', padding: 60}}>
-      <ActivityIndicator/>
-      <ActivityIndicator size={'large'}/>
-      <ActivityIndicator size={'large'} color={'red'}/>
-      <ActivityIndicator size={'large'} color={'red'} animating={false}/>
+      <Button title='Alert' onPress={()=> Alert.alert('Invalid Data!') }/>
+      <Button title='Alert 2' onPress={()=> Alert.alert('Invalid Data!', "DOB Incorrect") }/>
+      <Button title='Alert 3' onPress={()=> Alert.alert('Invalid Data 3!', "DOB Incorrect", 
+        [
+          {
+            text: 'Cancel',
+            onPress: () => console.log('Cancel Pressed')
+          },
+          {
+            text: 'ok',
+            onPress: () => console.log('ok Pressed')
+          },
+          {
+            text: 'no',
+            onPress: () => console.log('no Pressed')
+          },
+          
+          {
+            text: 'not ok',
+            onPress: () => console.log('not ok Pressed')
+          },
+        ]
+      ) }/>
     </View>
   );
 }
