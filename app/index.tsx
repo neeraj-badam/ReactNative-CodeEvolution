59658f327ab1399032d9dccc768e3ffcf6e8b014
neeraj-badam <neeraj.badam@gmail.com>
2025-02-26 17:08:14 -0500

    Pikachu Exercise

--- app/index.tsx
+++ app/index.tsx
@@ -1,49 +1,61 @@
-import { View, StyleSheet, Text, SafeAreaView, Platform } from 'react-native';
-import CustomButton from '@/components/CustomButton/CustomButton';
+import { SafeAreaView, StyleSheet, Platform, ScrollView} from 'react-native';
+
+import PokemonCard from '@/components/PokemonCard';
 
 export default function App(){
+  const charmanderData = {
+    name: "Charmander",
+    image: require("../assets/images/charmander.png"),
+    type: "Fire",
+    hp: 39,
+    moves: ["Scratch", "Ember", "Growl", "Leer"],
+    weaknesses: ["Water", "Rock"],
+  };
+
+  const squirtleData = {
+    name: "Squirtle",
+    image: require("../assets/images/squirtle.png"), // Replace with the actual image path
+    type: "Water",
+    hp: 44,
+    moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
+    weaknesses: ["Electric", "Grass"],
+  };
+
+  const bulbasaurData = {
+    name: "Bulbasaur",
+    image: require("../assets/images/bulbasaur.png"), // Replace with the actual image path
+    type: "Grass",
+    hp: 45,
+    moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
+    weaknesses: ["Fire", "Ice", "Flying", "Psychic"],
+  };
+
+  const pikachuData = {
+    name: "Pikachu",
+    image: require("../assets/images/pikachu.png"), // Replace with the actual image path
+    type: "Electric",
+    hp: 35,
+    moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
+    weaknesses: ["Ground"],
+  };
 
   return (
-    <SafeAreaView style={styles.safeContainer}>
-      <View style={styles.container}>
-        <View style={styles.box}>
-          <Text style={styles.text}>
-            Welcome {Platform.OS}
-            <CustomButton title="Press me" onPress={() => console.log('Button Pressed')} />
-          </Text>
-        </View>
-      </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <PokemonCard {...charmanderData} />
+        <PokemonCard {...squirtleData} />
+        <PokemonCard {...bulbasaurData} />
+        <PokemonCard {...pikachuData} />
+      </ScrollView>
     </SafeAreaView>
   );
 }
 
 
 const styles = StyleSheet.create({
-  safeContainer: {
-    flex: 1,
-    backgroundColor: 'plum',
-  },
   container: {
     flex: 1,
-    backgroundColor: 'plum',
-    paddingTop: Platform.OS == 'ios'? 25 : 0,
+    backgroundColor: '#f5f5f5',
+    paddingTop: Platform.OS === 'android' ? 25 : 0,
   },
-  box: {
-    padding: 20,
-  },
-  text: {
-    ...Platform.select({
-      ios: {
-        color: 'purple',
-        fontSize: 24,
-        fontStyle: 'italic',
-      },
-      android: {
-        color: 'blue',
-        fontSize: 30
-      },
-    }),
-    fontWeight: 'bold',
-    alignSelf: 'center',
-  }
 });
