991f488abd29471822ab0f862371c74acaaee9cf
neeraj-badam <neeraj.badam@gmail.com>
2025-02-27 15:24:33 -0500

    LOGIN

--- app/index.tsx
+++ app/index.tsx
@@ -1,40 +1,63 @@
-import { SafeAreaView, StyleSheet, Text, TextInput, StatusBar, Switch, View} from 'react-native';
+import { Platform, KeyboardAvoidingView, Image, SafeAreaView, StyleSheet, Text, TextInput, StatusBar, Switch, View, Button} from 'react-native';
 import { useState } from 'react';
 
 export default function App(){
-  const [name, setName] = useState('');
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({});
+
+  const validateForm = () => {
+    let errors = {};
+
+    if( !username ) errors.username = 'Username is required';
+    if( !password ) errors.password = 'Password is required';
+    
+    setErrors( errors );
+
+    return Object.keys( errors ).length === 0;
+  };
+
+  const handleSubmit = () => {
+    if( validateForm() ){
+      console.log( 'Submitted', username, password );
+      setUsername(''),
+      setPassword('');
+      setErrors({});
+    };
+  }
 
   return (
-    <SafeAreaView style={styles.container}>
-      <TextInput
-        style={styles.input}
-        value={name}
-        onChangeText={setName}
-        placeholder={`email@example.com`}
-        // secureTextEntry
-        // keyboardType='email-address'
-        // autoCorrect={false}
-        // autoCapitalize='none'
-      />
-      <TextInput
-        style={[styles.input, styles.multiLineText]}
-        placeholder={`Message`}
-        multiline
-      />
-      <Text style={styles.text}> My Name is {name} </Text>
-
-      <View style={styles.switchContainer}>
-        <Text style={styles.text}> Dark Mode </Text>
-        <Switch
-          value={isDarkMode}
-          onValueChange={() => setIsDarkMode( (prevState) => !prevState)}
-          trackColor={{ false: 'orange', true: 'violet' }}
-          thumbColor={`yellow`}
+    <KeyboardAvoidingView style={styles.container} behavior='padding' keyboardVerticalOffset={Platform.OS === 'ios' ? 100: 50}>
+      <View style={styles.form}>
+        <Image source={require('../assets/images/adaptive-icon.png')} style={styles.image}/>
+        <Text style={styles.label}>
+          Username
+        </Text>
+        <TextInput
+          placeholder='Enter your username'
+          style={styles.input}
+          value={username}
+          onChangeText={setUsername}
         />
+        {
+          errors?.username ? <Text style={styles.errorText}> {errors.username} </Text> : null
+        }
+        <Text style={styles.label}>
+          Password
+        </Text>
+        <TextInput
+          placeholder='Enter your password'
+          style={styles.input}
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+        {
+          errors?.password ? <Text style={styles.errorText}> {errors.password} </Text> : null
+        }
+        <Button title='Login' onPress={handleSubmit} />
       </View>
-
-    </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -42,27 +65,45 @@ export default function App(){
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+    backgroundColor: '#f5f5f5',
     paddingTop: StatusBar.currentHeight,
+  },
+  form: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 5,
+    fontWeight: 'bold',
   },
   input: {
     height: 40,
-    margin: 12,
-    padding: 10,
+    borderColor: '#ddd',
     borderWidth: 1,
-  },
-  text: {
-    fontSize: 30,
+    marginBottom: 15,
     padding: 10,
+    borderRadius: 5,
   },
-  multiLineText: {
-    minHeight: 100,
-    textAlignVertical: 'top',
+  image: {
+    width: 200,
+    height: 400,
+    alignSelf: 'center',
+    marginBottom: 50,
   },
-  switchContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    paddingHorizontal: 10,
+  errorText: {
+    color: 'red',
+    marginBottom: 10,
   },
 });
