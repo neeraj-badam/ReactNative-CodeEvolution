e8e0747718476f547547a678f4a2a80c7d12c164
neeraj-badam <neeraj.badam@gmail.com>
2025-03-01 14:05:55 -0500

    Drawer

--- app/index.tsx
+++ app/index.tsx
@@ -1,84 +1,29 @@
-import {Text} from 'react-native';
+import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from '@/screens/HomeScreen';
-import AboutScreen from '@/screens/AboutScreen';
-import { Pressable } from 'react-native';
 
-const Stack = createNativeStackNavigator();
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import DashboardScreen from '@/screens/DashboardScreen';
+import SettingsScreen from '@/screens/SettingsScreen';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    // <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'
-
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: '#6a51ae'
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-          // color: 'red',
-        },
-        headerRight: () => (
-          <Pressable
-            onPress={
-              () => alert('Button Pressed')
+    <>
+      <Drawer.Navigator>
+        <Drawer.Screen name='Dashboard' component={DashboardScreen} 
+          options={{
+            title:"My Dashboard",
+            drawerLabel: "Dashboard",
+            drawerActiveTintColor: 'red',
+            drawerActiveBackgroundColor: 'green',
+            drawerContentStyle: {
+              backgroundColor: '#c6cbef'
             }
-          >
-            <Text style={{color: 'white', fontSize: 16}}>
-              Menu
-            </Text>
-          </Pressable>
-        ),
-        contentStyle: {
-          backgroundColor: '#e8e4f3'
-        }
-      }}
-      >
-        <Stack.Screen name="Home" component={HomeScreen} options={{
-          title: 'Welcome Home',
-          headerStyle: {
-            backgroundColor: '#6a51ae'
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-            // color: 'red',
-          },
-          headerRight: () => (
-            <Pressable
-              onPress={
-                () => alert('Button Pressed')
-              }
-            >
-              <Text style={{color: 'white', fontSize: 16}}>
-                Menu
-              </Text>
-            </Pressable>
-          ),
-          contentStyle: {
-            backgroundColor: '#e8e4f3'
-          }
-        }}/>
-        <Stack.Screen 
-          name="About"
-          component={AboutScreen} initialParams={{
-            name:`Guest`
           }}
-          />
-
-          {/*
-          If we want to change the header name on the application this is one way and another option is 
-          options={
-            ({route}) => ({
-              title: route.params.name
-            })
-          }
-          />
-            */}
-      </Stack.Navigator>
-    // </NavigationContainer>
-  );
+        />
+        <Drawer.Screen name='Settings' component={SettingsScreen} />
+      </Drawer.Navigator>
+    </>
+  )
 }
